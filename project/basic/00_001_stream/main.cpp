c868196719daa8b6dd25d730b132493b9c063eed
EHmin <okchrisok@naver.com>
2023-07-13 23:30:40 +0900

    .

---
+++ project/basic/00_001_stream/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#define PI 3.141592
+using namespace std;
+string a;
+
+typedef int i; // 타입의 이름을 새롭게 정의 
+
+int main(){
+    i b = 10;
+    // cin >> a ;
+    // cout << a << b << '\n';
+    // cout << PI << '\n';
+
+    // int c, d;
+    // scanf("%d . %d", &c, &d); // 다음과 같은 트릭을 이용해서 소수점을 받아내는 것도 가능
+    // printf("%d %d", c, d);
+
+    // string s;
+    // getline(cin, s); // 개행문자를 같이 입력받고 싶은경우 
+    // cout << s << '\n';
+
+    // int T;
+    // string s;
+    // cin >> T;
+    // string bufferflush;
+    // getline(cin, bufferflush); // bufferflsuh를 이용하여 cin 뒤에 들어간 개행문자를 받아주어야 한다. 
+    // for( int i=0; i< T; i ++){
+    //     getline(cin, s);
+    //     cout << s << '\n';
+    // }
+
+    
+
+    return 0;
+}
