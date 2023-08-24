b4decfc346ae11f73b5f4e86817262d8cef1ed0c
EHmin <okchrisok@naver.com>
2023-08-24 11:35:46 +0900

    Data Structure queue, stack ,priority queue etc...

--- project/basic/00_005_DataStructure/main.cpp
+++ project/basic/00_005_DataStructure/main.cpp
@@ -2,8 +2,30 @@
 #include <vector>
 #include <list>
 #include <map>
+#include <set>
+#include <stack>
+#include <queue>
 
 using namespace std;
+
+struct Point
+{
+    int x, y;
+    Point(int x, int y) : x(x), y(y){}
+    Point() {x = -1; y = -1;}
+
+    bool operator < (const Point & other) const{// 항상 조건을 만족해주는 놈이 앞으로 가게 된다고 생각하자
+        if (x == other.x) return y > other.y; // y 에 대한 내림 차순 
+        return x < other.x; // x 에 대한 오름차순  
+    }
+};
+
+struct cmp{
+    bool operator()(Point a, Point b){
+        return a.x > b.x ; 
+    }
+};
+
 
 int main(){
     cout << "Vector" << endl;
@@ -121,6 +143,105 @@ int main(){
     if(mp.find("kkk") == mp.end()){ // 이렇게 확인하는 것이 생성되는 것을 막을수 있다. 시간 복잡도는...?
         mp["kkk"]  = 10; 
     }
+    cout << endl;
+
+    ///////////////////////////////
+    cout << "set()" << endl;
+    set<pair<string, int>> st;
+    st.insert({"this", 1});
+    st.insert({"this", 1});
+    st.insert({"this", 1});
+    st.insert({"next", 1});
+    cout << st.size()<< endl;
+    for( pair<string,int> a : st) cout << a.first << endl;
+    cout << endl;
+
+    ///////////////////////////////////////////////
+    cout <<"multiset" << endl;
+    multiset<int> mst;
+    for( int i = 1; i <= 5; i ++){
+        mst.insert(i);
+        mst.insert(i);
+    }
+    for( int a : mst) cout << a;
+    cout << endl<<endl;
+
+    ///////////////////////////////////////////////
+    cout << "stack" << endl;
+    stack<int> my_stack;
+    for( int i = 1; i <= 5; i ++){
+        my_stack.push(i);
+    }
+    while(my_stack.size()){ // cpp에서 0이면 거짓!
+        cout << my_stack.top(); 
+        my_stack.pop(); // 리턴이 안된다. 
+    }
+    cout << endl <<endl;
+
+    ///////////////////////////////////////////////
+    cout << "queue" << endl;
+    queue<int> my_queue;
+    for( int i = 1; i <= 5; i ++){
+        my_queue.push(i);
+    }
+    while(my_queue.size()){ // cpp에서 0이면 거짓!
+        cout << my_queue.front(); // stack은 top// queue 에서는 front
+        my_queue.pop(); // 앞에서 부터 나옴! 
+    }
+    cout << endl;
+
+    ///////////////////////////////////////////////////
+    deque<int> dq;
+    for( int i = 1; i <= 5; i ++){
+        dq.push_back(i);
+        dq.push_front(i);
+    }
+    for(int a : dq) cout << a;
+    cout << endl;
+
+    while(dq.size()){ 
+        cout << dq.front(); 
+        dq.pop_front(); 
+        dq.pop_back(); 
+    }
+    cout << endl;
     
+    ///////////////////////////////////////////////////
+    cout << "priority queue" << endl; // true 이면 아래쪽으로 향하도록 로직이 짜여져 있는듯
+    priority_queue<int> pq1; 
+    priority_queue<int, vector<int>, greater<int>> pq2; // 여기서는 오름 차순. sort() 에서 cmp로 적용시 내림 차순
+    priority_queue<int, vector<int>, less<int>> pq3;
+
+    for(int i = 1; i<= 5; i++){
+        pq1.push(i);
+        pq2.push(i);
+        pq3.push(i);
+    }
+
+    while(pq1.size()){
+        cout << pq1.top() << " : " << pq2.top() << " : " << pq3.top() << endl;
+        pq1.pop(); pq2.pop(); pq3.pop(); 
+    }
+
+    priority_queue<Point> pq_p;
+    pq_p.push(Point(1,3));
+    pq_p.push(Point(1,2));
+    pq_p.push(Point(3,1));
+    pq_p.push(Point(3,2));
+    pq_p.push(Point(3,3));
+    pq_p.push(Point(10,3));
+
+    cout << pq_p.top().x << " " << pq_p.top().y << endl; // x가 제일 큰 10이 튀어나옴
+
+    priority_queue<Point, vector<Point>, cmp> pq_c;
+    pq_c.push(Point(1,3));
+    pq_c.push(Point(1,2));
+    pq_c.push(Point(3,1));
+    pq_c.push(Point(3,2));
+    pq_c.push(Point(3,3));
+    pq_c.push(Point(10,3));
+
+    cout << pq_c.top().x << " " << pq_c.top().y << endl; // x기준 가장 작은것. 그 뒤로 y기준 작은거 튀어나옴 >> 1,2
+
 
 }
