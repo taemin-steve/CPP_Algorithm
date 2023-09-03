e4bbadd1f9b3e1ad71219844da1bef7dea66c92e
EHmin <okchrisok@naver.com>
2023-09-04 00:12:24 +0900

    2309 cpp complete

--- project/Week_01/01_01_2309/main.cpp
+++ project/Week_01/01_01_2309/main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
+#include <algorithm>
 
 using namespace std;
 int a[9];
+vector<int> ans;
 
-vector<int> combi(int n, int k, int start, vector<int> &v){
+void combi(int n, int k, int start, vector<int> &v){
     if(v.size() == k){
-        if( 100 == accumulate(v.begin(), v.end(), 0))
-        return v;
+        if( 100 == accumulate(v.begin(), v.end(), 0)){
+            ans =  v;
+        }
     }
+
     for(int i = start + 1; i < n; i++ ){
         v.push_back(a[i]);
         combi(n,k,i,v);
         v.pop_back();
     }
+
 }
 
 int main(){
@@ -23,7 +28,9 @@ int main(){
     for(int i = 0; i < 9; i++){
         cin >> a[i];
     }
-    
-    vector<int> ans = combi(9, 7, 0, v);
+    cout << endl;
+
+    combi(9, 7, -1, v);
+    sort(ans.begin(), ans.end());
     for( int i : ans) cout << i << endl;
 }   
