2e7213d93dcf621d25af4d105840519fedf108a3
EHmin <okchrisok@naver.com>
2023-09-13 17:21:25 +0900

    Fix CPP BOJ 1629 곱셈

--- project/Week_01/01_14_1629/main.cpp
+++ project/Week_01/01_14_1629/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <algorithm>
 
+typedef long long ll;
+
 using namespace std;
 int A, B, C;
+
+int DP(ll a, ll b){
+	if(b == 1) return a % C;
+
+	ll ret = DP(a, b/2) ;
+	ret = (ret * ret) % C; // 이미 계산한 것을 사용해 주자!
+	if(b % 2 != 0) ret = (ret * a) % C;
+	return ret; 
+}
 
 int main(){
 	cin >> A >> B >> C;
 
-	A = A % C;
+	cout << DP(A,B) << endl;
 
-	cout << pow(A,B) % C;	
+	return 0;
 }
+
+// (a*b) % c == (a%c) * (b%c)
