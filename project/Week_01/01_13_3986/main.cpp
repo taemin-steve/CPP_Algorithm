08a4015f3f41b091a11006a6304c7da434058064
EHmin <okchrisok@naver.com>
2023-09-13 17:04:44 +0900

    Update comment about stack

--- project/Week_01/01_13_3986/main.cpp
+++ project/Week_01/01_13_3986/main.cpp
@@ -12,10 +12,14 @@ int main() {
 		cin >> s; 
 		stack<char> stk;
 		for(char a : s){
-			if(stk.size() && stk.top() == a)stk.pop();
+			if(stk.size() && stk.top() == a)stk.pop(); // Stack에서 top, pop등을 할때 반드시 존재 하는지확인부터 해줘야한다 !
 			else stk.push(a);
 		} 
 		if(stk.size() == 0)ret++;
 	}
 	cout << ret << "\n"; 
-}
+}// https://www.acmicpc.net/problem/3986
+
+// 문제를 보고 모르겠으면 뒤집거나, 회전시켜보거나, 더 붙여 보자 
+// 두개가 만나면 터져 버리는 구조가 되면 좋을듯!
+// 짝짓기, 폭팔등이 있다면 stack의 구조일 가능성이 높다.
