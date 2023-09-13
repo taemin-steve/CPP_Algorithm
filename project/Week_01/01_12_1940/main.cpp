bb1ee67358e7c76bbaf5b0e96c1614ddba85a220
EHmin <okchrisok@naver.com>
2023-09-13 16:58:12 +0900

    Update comment about exceptions

--- project/Week_01/01_12_1940/main.cpp
+++ project/Week_01/01_12_1940/main.cpp
@@ -13,9 +13,9 @@ int main(){
         cin >> gou;
         mp[gou]++;
     }
-
+    // if(m > 20000) cout << 0 << cout; 시간 초과가 빡센경우 에외처리가 가능하다면 진행해 주자!
     for(auto i : mp){
-        if(mp.find(M - i.first) != mp.end()){
+        if(mp.find(M - i.first) != mp.end()){ // 세개까지는 그냥 콤비네이션 하자!
             ans += min(mp[M - i.first], mp[i.first]);
         }
     }
