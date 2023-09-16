99e90996927b3897ca658bd6aad37a1fa095543f
EHmin <okchrisok@naver.com>
2023-09-16 12:05:49 +0900

    Update refactoring BOJ 2178

--- project/Week_02/02_01_2178/main.cpp
+++ project/Week_02/02_01_2178/main.cpp
@@ -1,46 +1,39 @@
 #include <iostream>
 #include <queue>
-
-
+#include <tuple>
 using namespace std;
-
 int N, M;
-pair<int, int> p;
+int adj[104][104]; // 한번에 여유있게 잡아주고
+int visited[104][104];
+int x,y;
 queue<pair<int,int>> q;
-string s;
 
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
-
+int dy[4] = {-1,0,1,0};
+int dx[4] = {0,1,0,-1};
 
 int main(){
-    cin >> N >> M;
-    int m[N][M];  // 일단 이렇게 하고 뭐가 다른지 한번 확인해보자
-    
-    for( int i=0; i<N; i++){
-        cin >> s;
+    scanf("%d %d", &N, &M);
+    for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
-            m[i][j] = s[j] - '0';
+            scanf("%1d", &adj[i][j]); // 입출력에서, scanf랑 cin getline(cin, s) 이런것들 한번 모두 정리해 줘야 할거같음
         }
     }
-
-    q.push(make_pair(0,0)); // make_pair()는 특이한 함수! 이렇게 써서 바로 진행가능
-    m[0][0] = 1;
+    
+    q.push({0,0}); // 우리에게는 1,1 >> 0,0
+    visited[0][0] = 1;
 
     while(q.size()){
-        p = q.front(); 
-        q.pop();
-        
+        tie(y,x) = q.front();q.pop();
         for(int i=0; i<4; i++){
-            int ny = p.first + dy[i];
-            int nx = p.second + dx[i];
-
-            if(ny < 0 || ny >= N || nx < 0|| nx >=M) continue;
-            if(m[ny][nx] == 0 || m[ny][nx] != 1) continue;
-            m[ny][nx] = m[p.first][p.second] + 1;
-            q.push(make_pair(ny,nx));
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if( ny<0 || ny>=N || nx<0 || nx>=M || adj[ny][nx] == 0) continue;
+            if(visited[ny][nx]) continue;
+            visited[ny][nx] = visited[y][x] + 1;
+            q.push({ny,nx});
         }
     }
 
-    cout << m[N-1][M-1] << endl;
+    printf("%d", visited[N-1][M-1]);
+    return 0;
 }
